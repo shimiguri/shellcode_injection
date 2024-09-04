0f82e00a7feac00af40c07c5558b5836312d0fba
bizzi <179275521+shimiguri@users.noreply.github.com>
2024-09-04 20:01:37 +0200

    Create functions.c

---
+++ UD/basic_syscalls/functions.c
@@ -0,0 +1,129 @@
+#include <windows.h> 
+#include <stdbool.h> 
+#include <psapi.h>
+#include <stdlib.h>
+#include <wincrypt.h>
+#include <time.h>
+#include <stdio.h>
+#include <time.h>
+#define NT_FUNCTIONS
+#ifdef NT_FUNCTIONS
+typedef struct _UNICODE_STRING
+{
+    USHORT Length;
+    USHORT MaximumLength;
+    _Field_size_bytes_part_opt_(MaximumLength, Length) PWCH Buffer;
+} UNICODE_STRING, *PUNICODE_STRING;
+
+typedef struct _OBJECT_ATTRIBUTES
+{
+    ULONG Length;
+    HANDLE RootDirectory;
+    PUNICODE_STRING ObjectName;
+    ULONG Attributes;
+    PVOID SecurityDescriptor; // PSECURITY_DESCRIPTOR;
+    PVOID SecurityQualityOfService; // PSECURITY_QUALITY_OF_SERVICE
+} OBJECT_ATTRIBUTES, *POBJECT_ATTRIBUTES;
+
+typedef struct _CLIENT_ID
+{
+    HANDLE UniqueProcess;
+    HANDLE UniqueThread;
+} CLIENT_ID, *PCLIENT_ID;
+
+typedef struct _PS_ATTRIBUTE
+{
+    ULONG_PTR Attribute;
+    SIZE_T Size;
+    union
+    {
+        ULONG_PTR Value;
+        PVOID ValuePtr;
+    };
+    PSIZE_T ReturnLength;
+} PS_ATTRIBUTE, *PPS_ATTRIBUTE;
+typedef struct _PS_ATTRIBUTE_LIST
+{
+    SIZE_T TotalLength;
+    PS_ATTRIBUTE Attributes[1];
+} PS_ATTRIBUTE_LIST, *PPS_ATTRIBUTE_LIST;
+
+//__________/NTAPI FUNCTIONS\___________
+extern NTSTATUS NtOpenProcess(
+    OUT PHANDLE ProcessHandle,
+    IN ACCESS_MASK DesiredAccess,
+    IN POBJECT_ATTRIBUTES ObjectAttributes,
+    IN PCLIENT_ID ClientId OPTIONAL
+);
+
+extern NTSTATUS NtAllocateVirtualMemory(
+    IN HANDLE ProcessHandle,
+    IN OUT PVOID* BaseAddress,
+    IN ULONG ZeroBits,
+    IN OUT PSIZE_T RegionSize,
+    IN ULONG AllocationType,
+    IN ULONG Protect
+);
+
+extern NTSTATUS NtWriteVirtualMemory(
+    IN HANDLE ProcessHandle,
+    IN PVOID BaseAddress,
+    IN PVOID Buffer,
+    IN SIZE_T NumberOfBytesToWrite,
+    OUT PSIZE_T NumberOfBytesWritten OPTIONAL
+);
+
+extern NTSTATUS NtProtectVirtualMemory(
+    _In_      HANDLE ProcessHandle,
+    _Inout_   PVOID* BaseAddress,
+    _Inout_   PSIZE_T RegionSize,
+    _In_      ULONG NewProtect,
+    _Out_     PULONG OldProtect
+);
+
+extern NTSTATUS NtCreateThreadEx(
+    OUT PHANDLE ThreadHandle,
+    IN ACCESS_MASK DesiredAccess,
+    IN POBJECT_ATTRIBUTES ObjectAttributes OPTIONAL,
+    IN HANDLE ProcessHandle,
+    IN PVOID StartRoutine,
+    IN PVOID Argument OPTIONAL,
+    IN ULONG CreateFlags,
+    IN SIZE_T ZeroBits,
+    IN SIZE_T StackSize,
+    IN SIZE_T MaximumStackSize,
+    IN PPS_ATTRIBUTE_LIST AttributeList OPTIONAL
+);
+
+extern NTSTATUS NtWaitForSingleObject(
+    _In_ HANDLE Handle,
+    _In_ BOOLEAN Alertable,
+    _In_opt_ PLARGE_INTEGER Timeout
+);
+
+extern NTSTATUS NtFreeVirtualMemory(
+    _In_      HANDLE ProcessHandle,
+    _Inout_   PVOID* BaseAddress,
+    _Inout_   PSIZE_T RegionSize,
+    _In_      ULONG FreeType
+);
+extern NTSTATUS NtProtectVirtualMemory(
+    _In_ HANDLE ProcessHandle,
+    _Inout_ PVOID *BaseAddress,
+    _Inout_ PSIZE_T RegionSize,
+    _In_ ULONG NewProtect,
+    _Out_ PULONG OldProtect
+);
+
+extern NTSTATUS NtClose(
+    IN HANDLE Handle
+);
+
+extern NTSTATUS NtFreeVirtualMemory(
+    _In_ HANDLE ProcessHandle,
+    _Inout_ PVOID *BaseAddress,
+    _Inout_ PSIZE_T RegionSize,
+    _In_ ULONG FreeType
+);
+
+#endif
